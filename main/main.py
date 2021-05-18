7eb476e5db4a5e9181e603bfa4a03709363209a0
Eric YANG <eric10179@gmail.com>
2021-05-18 16:27:09 +0800

    課堂目前進度(2021/05/18)

---
+++ main/main.py
@@ -0,0 +1,77 @@
+import pygame
+from pathlib import Path
+from mymissile import MyMissile
+from player import Player
+
+parent_path = Path(__file__).parents[1]
+image_path = parent_path / 'res'
+icon_path = image_path / 'airplaneicon.png'
+pygame.display.set_caption("1942偽")
+icon = pygame.image.load(icon_path)
+pygame.display.set_icon(icon)
+
+pygame.init()
+screenHigh = 760
+screenWidth = 1000
+playground = [screenWidth, screenHigh]
+screen = pygame.display.set_mode((screenWidth, screenHigh))
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((50, 50, 50))
+running = True
+fps = 60
+movingScale = 600 / fps
+player = Player(playground=playground, sensitivity=movingScale)
+clock = pygame.time.Clock()
+Missile = []
+launchMissile = pygame.USEREVENT + 1
+createEnemy = pygame.USEREVENT + 2
+explosion = pygame.USEREVENT + 3
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == launchMissile:
+            m_x = player.xy[0] + 20
+            m_y = player.xy[1]
+            Missile.append(MyMissile(xy=(m_x, m_y), playground=playground, sensitivity=movingScale))
+            m_x = player.xy[0] + 80
+            Missile.append(MyMissile(xy=(m_x, m_y), playground=playground, sensitivity=movingScale))
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player.to_the_left()
+            if event.key == pygame.K_d:
+                player.to_the_right()
+            if event.key == pygame.K_s:
+                player.to_the_bottom()
+            if event.key == pygame.K_w:
+                player.to_the_top()
+            if event.key == pygame.K_SPACE:
+                m_x = player.x + 20
+                m_y = player.y
+                Missile.append(MyMissile(xy=(m_x, m_y), playground=playground, sensitivity=movingScale))
+                m_x = player.x + 80
+                Missile.append(MyMissile(playground, (m_x, m_y), movingScale))
+                pygame.time.set_timer(launchMissile, 400)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_d:
+                player.stop_x()
+            if event.key == pygame.K_s or event.key == pygame.K_w:
+                player.stop_y()
+            if event.key == pygame.K_SPACE:
+                pygame.time.set_timer(launchMissile, 0)
+
+    screen.blit(background, (0, 0))
+    Missile = [item for item in Missile if item.available]
+    for m in Missile:
+        m.update()
+        screen.blit(m.image, m.xy)
+    player.update()
+    screen.blit(player.image, player.xy)
+    pygame.display.update()
+    dt = clock.tick(fps)
+pygame.quit()
---
+++ main/player.py
@@ -0,0 +1,42 @@
+import math
+from pathlib import Path
+from typing import Union
+import pygame
+from pygame.surface import Surface, SurfaceType
+from gobject import GameObject
+
+
+class Player(GameObject):
+    def __init__(self, playground, xy=None, sensitivity=1):
+        GameObject.__init__(self, playground)
+        self._moveScale = 0.5 * sensitivity
+        __parent_path = Path(__file__).parents[1]
+        self.__player_path = __parent_path / 'res' / 'airplane.png'
+        self._image = pygame.image.load(self.__player_path)
+        self._center = self._x + self._image.get_rect().w / 2, self._y + self._image.get_rect().h / 2
+        self._radius = 0.3 * math.hypot(self._image.get_rect().w, self._image.get_rect().h)
+
+        if xy is None:
+            self._x = (self._playground[0] - self._image.get_rect().w) / 2
+            self._y = 3 * self._playground[1] / 4
+        else:
+            self._x = xy[0]
+            self._y = xy[1]
+        self._objectBound = (10, self._playground[0] - (self._image.get_rect().w + 10), 10,
+                             self._playground[1] - (self._image.get_rect().h + 10))
+
+    def get_image(self) -> Union[Surface, SurfaceType]:
+        return self._image
+
+    def update(self):
+        GameObject.update(self)
+        self._center = self._x + self._image.get_rect().w / 2, self._y + self._image.get_rect().h / 2
+
+    def collided_detect(self, enemies):
+        for m in enemies:
+            if self._collided_(m):
+                self._hp -= 10
+                self._collided = True
+                m.hp = -1
+                m.collided = True
+                m.available = False
