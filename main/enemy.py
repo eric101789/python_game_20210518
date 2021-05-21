e812aa21edfa8c992699718b3cf4e54e25f193cc
eric101789 <59027989+eric101789@users.noreply.github.com>
2021-05-21 17:40:46 +0800

    課堂目前進度(2021/05/21)_Explosion_10

--- main/enemy.py
+++ main/enemy.py
@@ -56,4 +56,5 @@ class Enemy(GameObject):
             self._available = False
         if self._y < self._objectBound[2]:
             self._y = self._objectBound[2]
+            self._collided = True
             self._available = False
